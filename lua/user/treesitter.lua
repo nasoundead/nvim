90a889d1f0918ae1d3fd204e62b45ecd5f9af863
nasoundead <nasoundead@163.com>
2022-08-24 23:15:23 +0800

    whb

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -12,8 +12,25 @@ configs.setup({
 		enable = true, -- false will disable the whole extension
 		disable = { "css" }, -- list of language that will be disabled
 	},
+	-- 启用增量选择
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+		  init_selection = '<CR>',
+		  node_incremental = '<CR>',
+		  node_decremental = '<BS>',
+		  scope_incremental = '<TAB>',
+		}
+	  },
 	autopairs = {
 		enable = true,
 	},
 	indent = { enable = true, disable = { "python", "css" } },
 })
+
+-- 开启 Folding
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+-- 默认不要折叠
+-- https://stackoverflow.com/questions/8316139/how-to-set-the-default-to-unfolded-when-you-open-a-file
+vim.wo.foldlevel = 99
